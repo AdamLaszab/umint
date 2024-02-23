fd680dfb1b897e8ae0e7ef420f712866b328a55c
AdamLaszab <adam.laszab@gmail.com>
2024-02-24 00:08:47 +0100

    first

---
+++ uloha1.m
@@ -0,0 +1,37 @@
+function answer = uloha1(x, d)
+    fun = @(x) 0.2*x.^4 + 0.2*x.^3 - 4*x.^2 + 10;
+
+    x_values = linspace(-6, 6, 100);
+    y_values = fun(x_values);
+
+    plot(x_values, y_values, 'LineWidth', 2);
+    hold on;
+
+    answer = [x, fun(x)];
+    while true
+        y = fun(x);
+        xL = x - d;
+        xP = x + d;
+        yL = fun(xL);
+        yP = fun(xP);
+
+        if (yL >= y && yP >= y)
+            answer = [x, y];
+            break;
+        end
+
+        if (yL < y)
+            x = xL;
+        end
+
+        if (yP < y)
+            x = xP;
+        end
+    end
+
+    plot(answer(1), answer(2), 'ro', 'MarkerSize', 10, 'LineWidth', 2);
+    
+
+
+    hold off;
+end
---
+++ uloha2.m
@@ -0,0 +1,22 @@
+matica=ones(2,10).*[-500;500];
+rate=0.6;
+for i = 1:5
+    firstpop = genrpop(10, [-500 -500 -500 -500 -500 -500 -500 -500 -500 -500; 500 500 500 500 500 500 500 500 500 500]);
+    for gen = 1:1000
+        fitness = testfn3(firstpop);
+        graph(i,gen)= min(fitness);
+        vyber = selbest(firstpop, fitness, [0 0 0 0 0 0 1 1 1 1]);
+        nahodny=selrand(firstpop,fitness,8);
+        koleso=selsus(firstpop,fitness,4);
+        nahodny=around(nahodny,0,0.9,matica);
+        nevybrany = selbest(firstpop, fitness, [1 1 1 1 0 0 0 0 0 0]);
+        krizeny = crossov(nevybrany, 2, 0);
+        mutovany = mutx(krizeny,rate,matica);
+        krizenyvybrany=crossov(vyber,2,0);
+        koniec = [mutovany; vyber;nahodny;krizeny;krizenyvybrany;koleso];
+        firstpop = koniec;
+    end
+    hold on
+    plot(graph(i,:));
+end
+hold off;
