7d66387d19b8edc0f72878b51bc95b16a91ec476
AdamLaszab <adam.laszab@gmail.com>
2024-02-28 15:51:54 +0100

    picovina predmet vyjebany

--- uloha3.m
+++ uloha3.m
@@ -1,15 +1,14 @@
-% parameters
 Points = [0,0; 77,68; 12,75; 32,17; 51,64; 20,19; 72,87; 80,37; 35,82;
           2,15; 18,90; 33,50; 85,52; 97,27; 37,67; 20,82; 49,0; 62,14; 7,60;
           100,100]';
 
-finalFitness = intmax;
+maxi = 10000;
 numGenes = size(Points, 2) - 2;
-finalOrder = zeros(1, numGenes);
+najlepsiaCesta = zeros(1, numGenes);
 populationSize = 30;
-maxGenerations = 500;
+maxGenerations = 750;
 maxIterations = 5;
-crossoverRate = 0.15;
+crossoverRate = 0.12;
 populationFitness = zeros(1, populationSize);
 
 for iteration = 1:maxIterations
@@ -32,63 +31,54 @@ for iteration = 1:maxIterations
                     matrix(row,:) = sum1;
             end
         populationFitness(:) = matrix;
-        [fitnessHistory(iteration, generation),idx]= min(populationFitness);
-        bestIndividual = NewPopulation(idx, :);
-
-        best = selbest(NewPopulation, populationFitness, [2 2]);
-        randomSelection = selrand(NewPopulation, populationFitness, 8);
-        susSelection = selsus(NewPopulation, populationFitness, 6);
-        tournamentSelection = seltourn(NewPopulation, populationFitness, ...
-                                        populationSize - size(best, 1) - size(randomSelection, 1) - size(susSelection, 1));
-
-        randomSelection = crosord(randomSelection, 1);
-        susSelection = crosord(susSelection, 1);
+        graph(iteration,generation) = min(populationFitness);
+        najlepsi=selbest(NewPopulation,populationFitness,[1]);
+        
+        best = selbest(NewPopulation, populationFitness, [1 1 1 1]);
+        randomSelection = selrand(NewPopulation, populationFitness, 7);
+        susSelection = selsus(NewPopulation, populationFitness, 7);
+        bestSwap = invord(best,crossoverRate);
+        tournamentSelection = seltourn(NewPopulation, populationFitness,populationSize-22);
+        randomSelection = invord(randomSelection, crossoverRate);
+        susSelection = invord(susSelection, crossoverRate);
         tournamentSelection = swappart(tournamentSelection, crossoverRate);
-        susSelection = swapgen(susSelection, crossoverRate);
-        tournamentSelection = invord(tournamentSelection, crossoverRate);
-        NewPopulation = [best; randomSelection; tournamentSelection; susSelection];
+        tournamentSelection = swapgen(tournamentSelection, crossoverRate);
+        NewPopulation = [best;bestSwap;randomSelection; tournamentSelection; susSelection];
 
     end
     
+    if maxi > graph(iteration, end)
+        maxi = graph(iteration, end);
+        najlepsiaCesta = najlepsi;
+    end
+
     figure(1);
-    plot(fitnessHistory(iteration, :));
-    
-    if iteration == 1
-        hold on
-    end
-    
-    if finalFitness > fitnessHistory(iteration, end)
-        finalFitness = fitnessHistory(iteration, end);
-        finalOrder = bestIndividual;
-    end
+    plot(graph(iteration,:));
+    hold on;
 
-    UpdatedPoints = Points;
-    for i = 1:numGenes   
-        UpdatedPoints(:, i + 1) = Points(:, finalOrder(i));
-    end
 
 end
 
 
 figure(2)
-plot(UpdatedPoints(1,:), UpdatedPoints(2,:), 'x-');
+konecneZnacenie = Points;
+for i = 1:numGenes   
+    konecneZnacenie(:, i + 1) = Points(:, najlepsiaCesta(i));
+end
+
+plot(konecneZnacenie(1,:), konecneZnacenie(2,:), 'O-');
 
 function uniqueRandomMatrix = generateUniqueRandomMatrix()
-    % Set the parameters
     numRows = 1;
     numCols = 18;
     minValue = 2;
     maxValue = 19;
 
-    % Generate a pool of unique random numbers
     allNumbers = minValue:maxValue;
     uniqueNumbers = datasample(allNumbers, numRows * numCols, 'Replace', false);
-
-    % Reshape the vector into a matrix
     uniqueRandomMatrix = reshape(uniqueNumbers, numRows, numCols);
 end
 
 function distance = calculateDistance(x1, y1, x2, y2)
-    % Calculate Euclidean distance
     distance = sqrt((x2 - x1)^2 + (y2 - y1)^2);
 end
